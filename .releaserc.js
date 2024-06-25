d2af5072c4340c4187e2e2d89681ef01a7c5ea02
luochongfei <luochongfei@126.com>
2024-06-26 01:35:40 +0800

    feat(index.js): index2222

--- .releaserc.js
+++ .releaserc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    branch: "master",
-    preset: "angular",
+    branch: 'master',
+    preset: 'angular',
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/npm",
-        "@semantic-release/git",
-        "@semantic-release/github",
-    ]
-}
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+        '@semantic-release/github',
+    ],
+};
